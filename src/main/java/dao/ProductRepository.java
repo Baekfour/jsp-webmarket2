649172b46576aaa6a271762d030fab32372f65de
Baekfour <bso416@hanmail.net>
2022-05-19 11:14:00 +0900

    Products save

---
+++ src/main/java/dao/ProductRepository.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import dto.Product;
+
+// Product에 대한 기능을 정의(상품정보 정리하는 클래스)
+public class ProductRepository {
+	private List<Product> products = new ArrayList<>();
+	
+	public ProductRepository() {
+		//원래는 DB에서 가져올 데이터이다
+		// 지금 DB를 배우지 않았기 때문에 그냥 3개를 만들었음
+		Product phone = new Product("1234", "iphone 6s", 800000);
+		phone.setDescription("4.5-inch, 1334X750 어쩌구");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
+		
+		Product notebook = new Product("1234", "iphone 6s", 1500000);
+		notebook.setDescription("4.5-inch, 1334X750 ");
+		notebook.setCategory("Smart Phone");
+		notebook.setManufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refubished");
+		
+		Product tablet = new Product("1234", "iphone 6s", 900000);
+		tablet.setDescription("4.5-inch, 1334X750 ");
+		tablet.setCategory("Smart Phone");
+		tablet.setManufacturer("Samsung");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
+		
+		products.add(phone);
+		products.add(notebook);
+		products.add(tablet);
+		
+	}
+	//모든 상품정보를 가져온다
+	public List<Product> getAllProducts(){
+		return products;
+	}
+}
--- src/main/webapp/welcome.jsp
+++ src/main/webapp/welcome.jsp
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="EUC-KR">
+<meta charset="UTF-8">
 <title>Insert title here</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -21,7 +21,7 @@
          </li>
       </ul>
    </nav>
-   <%!// 변수, 메서드 선언
+   <%!// 변수, 메소드 선언
    String greeting = "웹 쇼핑몰에 오신 것을 환영합니다";
    String tagline = "Welcome to Web Market!";%>
    <%
@@ -30,9 +30,9 @@
    %>
 
    <div class="p-5 bg-primary text-white">
-   <!-- container : 좌우 가운데 정렬 -->
+   <!-- container :좌우 가운데 정렬 -->
   <div class="container">
-  <!-- display-3 : 큰 글자중에 보통크기 -->
+  <!-- display-3 : 큰 글자중에 보통크기-->
     <h1 class="display-3">
       <%= greeting %>
     </h1>
@@ -44,7 +44,7 @@
     <%
     Date today = new Date();
     SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss a");
-    out.println("현재 접속 시간:" + format.format(today));
+    out.println("현재 접속 시간" + format.format(today));
     %>
   </div>
   <footer class="cotainer">
