37263ac7b6dbc733f40762fd14ee57a72d315f94
Baekfour <102018770+Baekfour@users.noreply.github.com>
2022-05-23 16:31:12 +0900

    mvc

--- src/main/java/dao/TodoRepository.java
+++ src/main/java/dao/TodoRepository.java
@@ -4,25 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 import dto.Todo;
 
-public class TodoRepository { // 저장소는 새로고침해도 계속 써야한다.
-	private static long currentId = 0;
-   private List<Todo> todos = new ArrayList<>();
-   
-   private static TodoRepository instance = new TodoRepository(); // 프로그램이 기동하자마자 만들고 다른애들이 쓸수있게 해줌
-   
-   public static TodoRepository getInstance() {
-      return instance;
-   }
-   
+public class TodoRepository {
+    private static long currentId = 0;
 
-   
-   public List<Todo> getTodos() {
-      return todos;
-   }
-   
-   public void addTodo(Todo todo) {
-	   todo.setId(currentId);
-	   currentId++;
-      todos.add(todo);
-   }
+    private List<Todo> todos = new ArrayList<>();
+
+    private static TodoRepository instance = new TodoRepository();
+
+    public static TodoRepository getInstance() {
+        return instance;
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public void addTodo(Todo todo) {
+        todo.setId(currentId);
+        currentId++;
+        todos.add(todo);
+    }
+
+    public void toggle(long id) {
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                todo.setDone(!todo.isDone());
+                break;
+            }
+        }
+    }
+
+    public void remove(long id) {
+        Todo removeTodo = null;
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                removeTodo = todo;
+                break;
+            }
+        }
+        todos.remove(removeTodo);
+    }
 }
